2027341087b7b5e9f1923fd2cc06787c3817f0a9
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-05-16 12:24:57 +0300

    Orai

---
+++ js/array.js
@@ -0,0 +1,28 @@
+/*
+Is duoto array reikia apskaiciuoti suma, kuri yra sudaryta is pirmo ir paskutinio to array nariu.
+*/
+
+const a = [4, 5];
+const b = [1, 2, 3, 4];
+const c = [99, 77, 55, 33, 22, 11, -11, -22, -33];
+const d = [-17, 55, 4, 5];
+const e = [-17, 55, 1, 2, 3, 4];
+const f = [-17, 55, 99, 77, 55, 33, 22, 11, -11, -22, -33];
+
+const xa = a[0] + a[a.length -1];
+const xb = b[0] + b[b.length -1];
+const xc = c[0] + c[c.length -1];
+const xd = d[0] + d[d.length -1];
+const xe = e[0] + e[e.length -1];
+const xf = f[0] + f[f.length -1];
+
+function suma(list) {
+    return list[0] + list[list.length -1];
+}
+
+console.log(suma(a));
+console.log(suma(b));
+console.log(suma(c));
+console.log(suma(d));
+console.log(suma(e));
+console.log(suma(f));
---
+++ js/math.js
@@ -0,0 +1,28 @@
+/*
+Reikia apskaiciuoti kam lygu duoto skaiciau penkis kartus didesne reiksme, kuri pries tai buvo sumazinta 7 vienetais.
+*/
+
+const a = 9;
+const b = 19;
+const c = -5;
+const d = -3.5;
+const e = 4;
+const f = 2000;
+
+const xa = (a-7)*5;
+const xb = (a-7)*5;
+const xc = (a-7)*5;
+const xd = (a-7)*5;
+const xe = (a-7)*5;
+const xf = (a-7)*5;
+
+function Math(skaicius) {
+    return (skaicius-7)*5;
+}
+
+console.log(xa, math(a));
+console.log(xb, math(b));
+console.log(xc, math(c));
+console.log(xd, math(d));
+console.log(xe, math(e));
+console.log(xf, math(f));
